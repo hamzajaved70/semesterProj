35eb58ea8fed90dbde8b1e91755c35740363cd88
Hamza Javed <hamzajaved70@gmail.com>
2017-05-16 00:21:07 +0500

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/help'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  devise_for :users
+
+
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "pages#home"
+
 end
